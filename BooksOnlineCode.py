5b63a8b3f6d36e63be7eeb3851647d51fe2522e8
Olando Plummer <olando.plummer@hotmail.com>
2023-11-05 23:06:45 -0500

    Add changes to BooksOnlineCode.py file books_online.csv file

--- BooksOnlineCode.py
+++ BooksOnlineCode.py
@@ -1,8 +1,9 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
 
 url = "http://books.toscrape.com/catalogue/1st-to-die-womens-murder-club-1_2/index.html"
 def extract_book_data(book_url):
@@ -23,13 +24,19 @@ def extract_book_data(book_url):
 
     book_data = {'book_url': book_url, 'universal_product_code': universal_product_code, 'price_including_tax': price_including_tax, 'price_excluding_tax': price_excluding_tax, 'quantity_available': quantity_available,'review_rating': review_rating, 'category': category, 'product_description': product_description, 'book_title': book_title, 'image_url': image_url}
 
+    headers = ["product_page_url", "universal_product_code(upc)", "book_title", "price_including_tax", "price_excluding_tax", "quantity_available", "product_description", "category", "review_rating", "image_url"]
+    headers_content = {'product_page_url': url, 'universal_product_code(upc)': universal_product_code, 'book_title': book_title, 'price_including_tax': price_including_tax, 'price_excluding_tax': price_excluding_tax, 'quantity_available': quantity_available, 'product_description': product_description, 'category': category, 'review_rating': review_rating, 'image_url': image_url}
 
+    if os.path.isfile("books_online.csv"):
+        with open("books_online.csv", "a", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writerow(headers_content)
+    else:
+        with open("books_online.csv","w",newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=headers)
+            writer.writeheader()
+            writer.writerow(headers_content)
 
-    with open("books_online.csv","a",newline="") as csvfile:
-        headers = ["product_page_url", "universal_product_code(upc)", "book_title", "price_including_tax", "price_excluding_tax", "quantity_available", "product_description", "category", "review_rating", "image_url"]
-        writer = csv.DictWriter(csvfile, headers)
-        writer.writeheader()
-        writer.writerow({'product_page_url': url, 'universal_product_code(upc)': universal_product_code, 'book_title': book_title, 'price_including_tax': price_including_tax, 'price_excluding_tax': price_excluding_tax, 'quantity_available': quantity_available, 'product_description': product_description, 'category': category, 'review_rating': review_rating, 'image_url': image_url})
 
     return book_data
 
@@ -57,49 +64,9 @@ def extract_book_category_url(category_url):
 
 categoryURL = "http://books.toscrape.com/catalogue/category/books/mystery_3/index.html"
 extract_book_category_url(categoryURL)
-=======
-import requests
-from bs4 import BeautifulSoup
-
-url = "http://books.toscrape.com/catalogue/sharp-objects_997/index.html"
-page = requests.get(url)
-soup = BeautifulSoup(page.content,'html.parser')
-book_title = soup.find("h1")
-table = soup.find(class_="table table-striped")
-universal_product_code = table.select('td')[0].get_text(strip = True)
-price_including_tax = table.select("td")[3].get_text(strip = True)
-price_excluding_tax = table.select("td")[2].get_text(strip = True)
-quantity_available = table.select("td")[5].get_text(strip = True)
-category = soup.find(href="../category/books/mystery_3/index.html").get_text(strip = True)
-product_description = soup.find("h2")
-review_rating = table.select("td")[6].get_text(strip = True)
-for image in soup.find_all('img'):
-    print(image['src'])
-
-image_url = image['src']
-print(url)
-print(universal_product_code)
-print(book_title.get_text(strip=True))
-print(price_including_tax)
-print(price_excluding_tax)
-print(quantity_available)
-print(product_description.get_text(strip = True))
-print(category)
-print(review_rating)
-print(image_url)
-
-
-
-import csv
-
-with open("books_online.csv","w",newline="") as csvfile:
-    headers = ["product_page_url", "universal_product_code(upc)", "book_title", "price_including_tax", "price_excluding_tax", "quantity_available", "product_description", "category", "review_rating", "image_url"]
-    writer = csv.DictWriter(csvfile, headers)
-    writer.writeheader()
-    writer.writerow({'product_page_url': url, 'universal_product_code(upc)': universal_product_code, 'book_title': book_title, 'price_including_tax': price_including_tax, 'price_excluding_tax': price_excluding_tax, 'quantity_available': quantity_available, 'product_description': product_description, 'category': category, 'review_rating': review_rating, 'image_url': image_url})
 
 
 
 
 
->>>>>>> origin/master
+#>>>>>>> origin/master
